30e2c93b3f8f22144400b5b8a9a130b3539726d8
shenyan <tank1018702@sina.com>
2018-12-25 16:41:08 +0800

    2018/12/25

---
+++ VEG_Test/Assets/Scenes/Move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        ControlUpdate();
+    }
+    void ControlUpdate()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, 20f), 0.1f);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, -20f), 0.1f);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.identity, 0.1f);
+        }
+    }
+}
